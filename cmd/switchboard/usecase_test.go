ee6b16fbe99a6abb6091cb5c7b6fb35f28f5e6e5
Akira Masuda <masuda.akira1014@gmail.com>
2024-12-13 23:36:39 +0900

    rm debug log

--- cmd/switchboard/usecase_test.go
+++ cmd/switchboard/usecase_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log/slog"
 	"os"
 	"testing"
 	"time"
@@ -51,12 +50,10 @@ func TestMain(t *testing.T) {
 					Return(&switchboard.XPost{
 						ID: "1111111111111111111",
 					}, nil)
-				slog.Warn("test")
 				mockXCli.EXPECT().Post(gomock.Any(), gomock.Regex("test2.*")).
 					Return(&switchboard.XPost{
 						ID: "2222222222222222222",
 					}, nil)
-				slog.Warn("test2")
 			},
 			wantErr: false,
 		},
